b62d6fa465ac936aba74960803ce24f06706fb9f
Bhushan Saini <bhushan.saini@wuh-group.com>
2024-09-11 16:00:29 +0530

    required cganges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,17 @@ import { Table } from './components/table-component/table.component';
 export class AppComponent {
   title = 'exel-visualization';
   public value1!: number;
-  public value2!: number;
+  public value2!: number[];
+  public value3!: number;
 
   public response1!: Table[];
   public response2!: Table[];
   public response3!: Table[];
+  public listEqs: any;
 
   public formControl1 = new FormControl<number | null>(null)
-  public formControl2 = new FormControl<number | null>(null)
+  public formControl2 = new FormControl<string | null>(null)
+  public formControl3 = new FormControl<number| null>(null)
 
 
   constructor(private dataService: DataService) {}
@@ -31,7 +34,12 @@ export class AppComponent {
 
     this.formControl2.valueChanges.subscribe(r=>{
       if(r)
-      this.value2 = r
+        this.value2 = r.replaceAll(' ', ',').split(',').filter(x=> !Number.isNaN(x)).map(x=> parseInt(x))
+    })
+
+    this.formControl3.valueChanges.subscribe(r=>{
+      if(r)
+        this.value3 = r
     })
   }
 
@@ -41,12 +49,13 @@ export class AppComponent {
       this.response1 = [r]
     });
 
-    // this.dataService.getByMultipleEquipmentNoSap(this.value2).subscribe((r: any) => {
-    //   this.response2=r
-    // });  
+    this.dataService.getByMultipleEquipmentNoSap(this.value2).subscribe((r: any) => {
+      this.response2=r
+    });  
     
-    this.dataService.getByMaterialNo(this.value2).subscribe((r: any) => {
+    this.dataService.getByMaterialNo(this.value3).subscribe((r: any) => {
       this.response3=r
+      this.listEqs = JSON.stringify(r.map((x: any)=>x.EquipmentNumber))
     });    
   }
 }
